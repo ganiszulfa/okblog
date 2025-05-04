b96a90145317277542347802f2ea987d5d3e7333
Ganis <615326+ganiszulfa@users.noreply.github.com>
2025-05-04 09:16:40 +0700

    Add favicon support for admin and web interfaces
    - Added a new SVG favicon file for both admin and web applications.
    - Updated index.html in the admin directory to link the new favicon.
    - Modified nuxt.config.js in the web directory to reference the SVG favicon instead of the previous ICO format.

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
         { name: 'description', content: '' }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
       ]
     }
   },
