e8004bf3b658392aefc9d124e08720bdbe52c76b
Ganis <615326+ganiszulfa@users.noreply.github.com>
2025-05-14 10:53:18 +0700

    Update build and deployment scripts for additional Docker build arguments
    - Modified build_and_push.sh to ensure proper output of additional build arguments.
    - Updated admin-deploy.yml to set additional build arguments for environment variables, enhancing the build process.

--- build_and_push.sh
+++ build_and_push.sh
@@ -7,7 +7,7 @@ rm /tmp/build_args || echo OK
 env >/tmp/build_args
 echo "--build-arg \""$(cat /tmp/build_args | sed -z 's/\n/" --build-arg "/g')"IGNORE_VAR=IGNORE_VAR\"" >/tmp/build_args
 BUILD_ARGS=$(cat /tmp/build_args)
-echo "ADDITIONAL_BUILD_ARGS: $ADDITIONAL_BUILD_ARGS" || echo "No additional build args"
+echo "ADDITIONAL_BUILD_ARGS: $ADDITIONAL_BUILD_ARGS "
 COMMAND="docker build -t $FULL_IMAGE_NAME $ADDITIONAL_BUILD_ARGS -f ./Dockerfile $BUILD_ARGS --no-cache ."
 /bin/bash -c "$COMMAND"
 docker push $FULL_IMAGE_NAME
